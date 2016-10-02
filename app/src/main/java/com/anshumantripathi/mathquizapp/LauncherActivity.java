f10545a8817c30241ffebc1aa2e7cc427b738e5b
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2016-10-01 21:10:59 -0700

    Added on back press functionality.

--- app/src/main/java/com/anshumantripathi/mathquizapp/LauncherActivity.java
+++ app/src/main/java/com/anshumantripathi/mathquizapp/LauncherActivity.java
@@ -2,13 +2,18 @@ package com.anshumantripathi.mathquizapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.annotation.BoolRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LauncherActivity extends AppCompatActivity {
+
+    Boolean doublePressToExit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,7 @@ public class LauncherActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LauncherActivity.this, QuizActivity.class);
-                intent.putExtra("operation","add");
+                intent.putExtra("operation", "add");
                 startActivity(intent);
 
             }
@@ -37,7 +42,7 @@ public class LauncherActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LauncherActivity.this, QuizActivity.class);
-                intent.putExtra("operation","sub");
+                intent.putExtra("operation", "sub");
                 startActivity(intent);
             }
         });
@@ -47,7 +52,7 @@ public class LauncherActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LauncherActivity.this, QuizActivity.class);
-                intent.putExtra("operation","mul");
+                intent.putExtra("operation", "mul");
                 startActivity(intent);
             }
         });
@@ -62,5 +67,29 @@ public class LauncherActivity extends AppCompatActivity {
                 startActivity(startMain);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doublePressToExit) {
+            Intent startMain = new Intent(Intent.ACTION_MAIN);
+            startMain.addCategory(Intent.CATEGORY_HOME);
+            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startMain);
+        }
+
+        this.doublePressToExit = true;
+
+        final Toast toast = Toast.makeText(LauncherActivity.this, "Press BACK again to exit", Toast.LENGTH_SHORT);
+        toast.show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doublePressToExit = false;
+                toast.cancel();
+            }
+        }, 2000);
+
     }
 }
